8f43d7b1a0eaa79888a7828735d8dc7775ea721d
AnangM <social.anangmb@gmail.com>
2019-05-14 20:28:07 +0700

    added a bunch of stuff

---
+++ index.php
@@ -0,0 +1,27 @@
+<!-- This line (until the next comment) -->
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="theme-color" content="#f2629d">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./lib/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/main.css">
+    <title>TravelKuy</title>
+</head>
+<!-- Until This Line  IS A MUST -->
+<body>
+    <!-- Navbar     -->
+    <nav class="navbar navbar-dark main-bg">
+    <a class="navbar-brand" href="./">
+        <img height="30px" class="d-inline-block align-top" src="./images/icon/map.svg" alt="">
+        TravelKuy
+    </a>
+    </nav>
+<!-- JS -->
+<script src="./lib/js/bootstrap.min.js"></script>
+<script src="./lib/js/jquery-3.4.1.min.js"></script>
+<script src="./lib/js/popper.min.js"></script>
+</body>
+</html>
