bc9cc3713f4cfa2ab2974a6863506dba8321d985
AnangM <social.anangmb@gmail.com>
2019-05-17 20:27:57 +0700

    delete lib

--- index.php
+++ index.php
@@ -13,11 +13,30 @@
 <!-- Until This Line  IS A MUST -->
 <body>
     <!-- Navbar     -->
-    <nav class="navbar navbar-dark main-bg">
+    <nav class="navbar navbar-dark  main-bg">
     <a class="navbar-brand" href="./">
         <img height="30px" class="d-inline-block align-top" src="./images/icon/map.svg" alt="">
         TravelKuy
     </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+  </div>
     </nav>
 <!-- JS -->
 <script src="./lib/js/bootstrap.min.js"></script>
