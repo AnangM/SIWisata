483c383c6e44dec7b70b23cea9b9a91d24439939
AnangM <social.anangmb@gmail.com>
2019-05-27 23:11:15 +0700

    add delete

--- admin/delete-destinasi.php
+++ admin/delete-destinasi.php
@@ -1,4 +1,16 @@
 <?php
+session_start();
+if (!isset($_SESSION['admin'])) {
+    header('Location:index.php');
+} else {
+    require $_SERVER['DOCUMENT_ROOT'].'/connect.php';
+    $id = $_POST['id'];
+    $sql = "delete from wisata_lokasi where id='" . $id . "'";
 
-
-?>
+    if ($conn->query($sql) === TRUE) {
+        header("Location: /admin/destinasi.php");
+        exit();
+    } else {
+        echo "Error menghapus data : " . $conn->error;
+    }
+}
